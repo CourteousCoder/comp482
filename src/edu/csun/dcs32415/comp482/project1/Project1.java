79f9c8582d017801cc8da6fda9b5289a393e2f5e
Daniel Schetritt <Daniel.Schetritt@live.com>
2017-09-12 18:05:28 -0700

    Added file support

--- src/edu/csun/dcs32415/comp482/project1/Project1.java
+++ src/edu/csun/dcs32415/comp482/project1/Project1.java
@@ -1,10 +1,26 @@
 package edu.csun.dcs32415.comp482.project1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Project1 {
     public static void run() {
         SortTester tester = new SortTester();
-        System.out.println(tester.getExperiment1());
+        System.out.println("=================================");
+        String e1 = tester.getExperiment1();
+        String e2 = tester.getExperiment2();
+        System.out.println(e1);
         System.out.println("---------------------------------");
-        System.out.println(tester.getExperiment2());
+        System.out.println(e2);
+
+        try{
+            FileWriter f = new FileWriter("exp1.csv");
+            f.write(e1);
+            f = new FileWriter("exp2.csv");
+            f.write(e2);
+        }
+        catch (IOException e) {
+            System.out.println("File write error.");
+        }
     }
 }
